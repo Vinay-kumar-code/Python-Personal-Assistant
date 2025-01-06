688fc44abde1b2d5aadfa90e861442b4c85572e6
Vinay Kumar <vinaykumar.balisetti@gmail.com>
2025-01-06 14:59:16 +0530

    Delete Gemini.py

--- Gemini.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="api_key")
-a=input("Type your prompt:- ")
-model = genai.GenerativeModel("gemini-2.0-flash-exp")
-response = model.generate_content(a)
-print(response.text)
